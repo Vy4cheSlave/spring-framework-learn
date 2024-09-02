73aed06d7dbe1a5df871d82166ccb0909e915faa
Vy4cheSlave <slav.subocheff@yandex.ru>
2024-09-02 18:43:11 +0700

    Dependency Injection Part 2 with setter

--- src/main/java/authorization/App.java
+++ src/main/java/authorization/App.java
@@ -12,9 +12,11 @@ public class App{
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        // DE with XML-conf
+        // DE with setter
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.printf("%d\n", musicPlayer.getVolume());
     }
 } 
--- src/main/java/authorization/MusicPlayer.java
+++ src/main/java/authorization/MusicPlayer.java
@@ -2,10 +2,34 @@ package authorization;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
     // IoC
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public MusicPlayer() {} // удаляется после определения своего конструктора
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 
     public void playMusic() {
